c05fc2636343daa261e07adbe1fffc9609ead5a4
D'elegance Lane <delegancelane@gmail.com>
2021-01-02 17:57:59 -0600

    One page app
    Light App

--- LightApp/ViewController.swift
+++ LightApp/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
+@IBOutlet var lightButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+    
     }
 
+@IBAction func buttonPressed(_ sender: Any) {
+    lightOn.toggle()
+    updateUI()
+}
+    
+    func updateUI() {
+        if lightOn {
+        view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+        } else {
+        view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+    }
+    
+}
 
 }
 
